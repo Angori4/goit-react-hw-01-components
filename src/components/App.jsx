de3cf45575d8035eb5f5471ee5e4651d54e6eb06
Angori4 <110497521+Angori4@users.noreply.github.com>
2023-02-16 00:01:35 +0200

    Profile_update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json'
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json'
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json'
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile propsuser={user} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
